f89db1d5a774265a671f1e45d2fce2709111f558
Shao-Ching Huang <schuang@users.noreply.github.com>
2019-10-29 23:27:54 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+pushd /tmp
+wget https://github.com/jgm/pandoc/releases/download/2.7.3/pandoc-2.7.3-linux.tar.gz
+tar xvfz pandoc-2.7.3-linux.tar.gz
+export PATH=/tmp/pandoc-2.7.3/bin/:$PATH
+popd
+
 pushd sphinx
 make html
 make latexpdf
