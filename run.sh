b3345358452e4fbf1e5d49fa498d4261b4bce53f
Shao-Ching Huang <schuang@users.noreply.github.com>
2019-10-29 23:39:42 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -9,10 +9,10 @@ popd
 pushd sphinx
 make html
 make latexpdf
-cp build/latex/report.pdf build/html
+cp -vf build/latex/report.pdf build/html
 popd
 
 pushd slides
 make
-cp slides.pdf ../sphinx/build/html
+cp -vf slides.pdf ../sphinx/build/html
 popd
